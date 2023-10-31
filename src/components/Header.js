60ed1aacda8264c9f24dac1fa0ed5c5e9434fa45
takapia95 <113641497+takapia95@users.noreply.github.com>
2023-10-31 02:24:19 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-        <div class="container text-center">
-            <div class="row align-items-start">
-                <div class="col">
-                Volcano Tracker (Powered by NASA) | Group 3
+        <div class = "container text-center">
+            <div class = "row align-items-start">
+                <div class = "col">
+                Natural disaster tracker (Powered by NASA) | Group 3
                 </div>
-                <div class="col">
+                <div class = "col">
                 Philip Nguyen | Ai Kha Phi Tran
                 </div>
             </div>
