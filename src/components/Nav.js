ca51993cdd8d8e5dc4954aa3dcc742f511425d02
takapia95 <113641497+takapia95@users.noreply.github.com>
2023-10-31 02:23:11 -0400

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Nav = () => {
   return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary nav-fill">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src='https://media.tenor.com/LYftKBBe2csAAAAi/earth-planet.gif' height={30} width={30} />
+        <nav class = "navbar navbar-expand-lg bg-body-tertiary nav-fill">
+        <div class = "container-fluid">
+            <a class = "navbar-brand">
+                <img src = 'https://media.tenor.com/LYftKBBe2csAAAAi/earth-planet.gif' height = {30} width = {30} />
             </a>
 
-            <div class="collapse navbar-collapse btn-3" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link active fw-bold" aria-current="page" href="#">trackme</a>
+            <div class = "collapse navbar-collapse btn-3" id = "navbarNav">
+            <ul class = "navbar-nav">
+                <li class = "nav-item">
+                    <a class = "nav-link active fw-bold" href = "https://github.com/takapia95/Group_3/tree/main">TrackMe</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="https://api.nasa.gov/">NASA APIs</a>
+                <li class = "nav-item">
+                    <a class = "nav-link active fw-bold" href = "https://api.nasa.gov/">NASA APIs</a>
                 </li>
             </ul>
-            <h4> hurricanes and wildfires tracker</h4>
+            <h1>Natural disaster tracker</h1>
             </div>
         </div>
         </nav>
